f6e31abaa476981121497111cf5e03d9d8d3521b
iliav <iliav@hurdit.com>
2018-01-09 10:45:55 -0500

    Created index route with reducer and action creator

--- src/index.js
+++ src/index.js
@@ -3,34 +3,19 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
-import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import PostsIndex from './components/posts_index';
 
-class Hello extends React.Component {
-  render() {
-    return (
-      <div>Hello!</div>
-    )
-  }
-}
-
-class Bye extends React.Component {
-  render() {
-    return (
-      <div>Bye!</div>
-    )
-  }
-}
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/hello" component={Hello} />
-        <Route path="/bye" component={Bye} />
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>
